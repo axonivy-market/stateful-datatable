71baafb886e541e9590e88e3cb4fd885b4072a6d
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-10-25 13:31:12 +0200

    rewrite test to use fixture instead of custom login

--- stateful-datatable-test/src_test/com/axonivy/demo/statefuldatatable/test/StatefulDatatableDemoWebIT.java
+++ stateful-datatable-test/src_test/com/axonivy/demo/statefuldatatable/test/StatefulDatatableDemoWebIT.java
@@ -5,7 +5,6 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.inject.Named;
 
@@ -17,6 +16,7 @@ import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
+import com.axonivy.ivy.webtest.engine.WebAppFixture;
 
 import ch.ivyteam.ivy.environment.IvyTest;
 import ch.ivyteam.ivy.security.IUser;
@@ -28,7 +28,7 @@ import ch.ivyteam.ivy.security.IUser;
  * 3_ start "showDatatableRepo" process
  * 4_ verify that the table of "Products" is displayed.
  * 5_ add a New "Product" into the business repo.
- * 
+ *
  */
 @IvyTest
 @IvyWebTest(headless = true)
@@ -37,20 +37,15 @@ public class StatefulDatatableDemoWebIT {
 
 	@Test
 	@Order(1)
-	public void createTestData() {
+	public void createTestData(WebAppFixture fixture, @Named("tester") IUser tester) {
 		// valid links can be copied from the start page of the internal web-browser
 		open(EngineUrl.createProcessUrl("stateful-datatable-demo/183AC82DD4753247/createTestData.ivp"));
+		fixture.login(tester.getName(), "tester");
 	}
 
 	@Test
 	@Order(2)
-	public void showDatatableRepo(@Named("tester") IUser tester) {
-		assertThat(tester.getDisplayName()).isEqualTo("Tester");
-		
-		open(EngineUrl.base() + "default-workflow/faces/loginTable.xhtml");
-
-		$(By.xpath("//tr[@data-rk='Tester']")).$(By.tagName("td")).shouldBe(visible).click();
-		
+	public void showDatatableRepo() {
 		// valid links can be copied from the start page of the internal web-browser
 		open(EngineUrl.createProcessUrl("stateful-datatable-demo/183AC82DD4753247/showDatatableRepo.ivp"));
 		// verify that the registration was successful.
@@ -66,28 +61,28 @@ public class StatefulDatatableDemoWebIT {
 		$(By.id("form:productTable:addProduct")).shouldBe(enabled).click();
 		// fill new customer form
 		$(By.id("addProductDialogForm:productName")).sendKeys("New test product");
-		
+
 		$(By.id("addProductDialogForm:status")).shouldBe(enabled).click();
 		$(By.id("addProductDialogForm:status_1")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:validThrough_input")).shouldBe(enabled).click();
 		$(By.id("addProductDialogForm:validThrough_panel")).$(By.linkText("24")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:orderDate_input")).shouldBe(enabled).click();
 		$(By.id("addProductDialogForm:orderDate_panel")).$(By.linkText("25")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:deliveryDate_input")).shouldBe(enabled).click();
 		$(By.id("addProductDialogForm:deliveryDate_panel")).$(By.linkText("26")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:quantity_input")).sendKeys("10");
-		
+
 		$(By.id("addProductDialogForm:quality")).shouldBe(enabled).click();
 		$(By.id("addProductDialogForm:quality_1")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:onSale")).shouldBe(enabled).click();
-		
+
 		$(By.id("addProductDialogForm:addProduct")).shouldBe(enabled).click();
-		
+
 		// verify that the registration was successful.
 		$(By.id("form:productTable")).shouldBe(visible, text("List of products"));
 	}
